57b565549487a61fcf3c4aa6f32ff97922006365
MikolajKubek <mikolajkubik1998@gmail.com>
2020-06-06 00:43:15 +0200

    initial commit, create basic backend, declare models

---
+++ backend/CookBookApp/CookBook/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    fats = models.FloatField(default=0)
+    carbohydrates = models.FloatField(default=0)
+    proteins = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.name
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    description = models.CharField(max_length=200)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    ingredients = models.ManyToManyField(Ingredient, through='RecipeIngredientAssociative')
+
+    def __str__(self):
+        return self.title
+
+class RecipeIngredientAssociative(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    amount = models.FloatField()
+
+class Comment(models.Model):
+    content = models.CharField(max_length=200)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.author}: {self.content}"
---
+++ backend/CookBookApp/CookBook/serializers.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.models import User
+from .models import Ingredient, Recipe, Comment, RecipeIngredientAssociative
+from rest_framework import serializers
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
+
+
+class RecipeIngredientAssociativeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RecipeIngredientAssociative
+        fields = ['id', 'recipe', 'ingredient', 'amount']
+
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = ['id', 'name', 'fats', 'carbohydrates', 'proteins']
+
+class RecipeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Recipe
+        fields = ['id', 'title', 'description', 'author']
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id', 'content', 'author', 'recipe']
